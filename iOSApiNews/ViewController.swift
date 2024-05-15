3f166fbd543f75dd6edf21f40706e83321f7e77f
Higor <92187145+HigorCris21@users.noreply.github.com>
2024-05-15 16:37:27 -0300

    Iniciando Aplicativo

---
+++ iOSApiNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSApiNews
+//
+//  Created by Higor  Lo Castro on 15/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
